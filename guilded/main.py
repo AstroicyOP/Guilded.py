8369d98f78086ed405b7d9698e8e51bda659d082
Astroicy <109760180+AstroicyOP@users.noreply.github.com>
2022-09-20 12:59:49 +0200

    guilded bot example
    Maybe in future it will work, until Guilded release thier api docs.

---
+++ guilded/main.py
@@ -0,0 +1,12 @@
+import guilded
+from guilded.ext import commands
+
+intents = guilded.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=">", intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run("token")
